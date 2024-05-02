d2650d68db7940e8c269f6801ed4c019269b4dd8
rased <rased100@gmail.com>
2024-05-02 12:33:39 +0600

    sadakah

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { getBooks, getChapters, getHadith, getSection } from "./api/HadisData";
 import Sidebar from "./components/Sidebar";
+import Settings from "./components/Settings";
 
 export default async function Home() {
   const books = await getBooks();
@@ -9,11 +10,16 @@ export default async function Home() {
   const hadith = await getHadith();
   return (
     <main className="pt-[20px] pl-[20px] pr-[20px]">
-      <div className="flex">
+      <div className="flex justify-end">
         <div className="hidden xl:flex">
           <Sidebar books={books} chapters={chapters} />
+          <Sidebar books={books} chapters={chapters} />
+          <Sidebar books={books} chapters={chapters} />
         </div>
-        <p>hadis collections</p>
+        <div className="ml-5">
+          <Settings />
+        </div>
+        {/* <p>hadis collections</p> */}
       </div>
     </main>
   );
